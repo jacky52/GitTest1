394f1fd1728dd09a94d89b9fdadeb38c4674f327
ruide.zhao <ruide.zhao@emagroup.cn>
2015-06-25 16:27:16 +0800

    just for test Git

--- app/src/main/java/com/example/ruidezhao/gittest/AddTest.java
+++ app/src/main/java/com/example/ruidezhao/gittest/AddTest.java
@@ -1,7 +1,13 @@
 package com.example.ruidezhao.gittest;
 
+import android.util.Log;
+
 /**
  * Created by ruide.zhao on 2015/6/25.
  */
 public class AddTest {
+
+    public AddTest(){
+        Log.d("","this is a constructor");
+    }
 }
