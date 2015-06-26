3f12e1223dafe291e1e2c7dace110d426849d68a
jacky52 <eric_john@126.cn>
2015-06-26 11:43:13 +0800

    test Git Push 777777777

--- app/src/main/java/com/example/ruidezhao/gittest/AddTest.java
+++ app/src/main/java/com/example/ruidezhao/gittest/AddTest.java
@@ -15,6 +15,6 @@ public class AddTest {
         Log.d("","I'm testing push herehhhhhhhhhhhhhhhhhhhhhhhhh");
         Log.d("","I'm testing push herekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
         Log.d("","I'm testing push lllllllllllllllllllllllllllllllll");
-        Log.d("","I'm testing push zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        Log.d("","I'm testing push zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
     }
 }
